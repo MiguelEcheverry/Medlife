2f89d852ddd188d71813b4c856c18496edb85bcf
Miguel Echeverry <miguel2eche.tic@gmail.com>
2026-06-17 14:27:03 -0500

    clase infernal i (organizacion)

--- medlife/src/App.jsx
+++ medlife/src/App.jsx
@@ -1,23 +1,18 @@
-import {userState} from 'react'
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import Inicio from './Paginas/inicio';
-import Contacto from './Paginas/contacto';
-import Registro from './Paginas/registro';
-
+import { useState } from 'react'
+import {  BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Inicio from './Paginas/inicio'
+import Contacto from './Paginas/contacto'
+import Registro from './Paginas/registro'
 function App() {
   return (
     <Router>
       <Routes>
-        {/* Redirección para la ruta raíz */}
-        <Route path="/" element={<Navigate to="/inicio" replace />} />
-        
-        {/* Rutas (se recomienda usar minúsculas por convención) */}
-        <Route path="/inicio" element={<Inicio />} />
-        <Route path="/contacto" element={<Contacto />} />
-        <Route path="/registro" element={<Registro />} />
+        <Route path="/Inicio" element={<Inicio />} />
+        <Route path="/Contacto" element={<Contacto />} />
+        <Route path="/Registro" element={<Registro />} />
       </Routes>
     </Router>
-  );
-}
+  )
+}  
 
-export default App;
+export default App
